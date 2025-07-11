baa1b88ca6e8faf1a0ba4c7a8c93904369b52d50
yash <yashraw111@gmail.com>
2025-07-11 10:38:08 +0530

    change origin

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,8 @@ app.use(express.urlencoded({extended:true}))
 //     origin: "*", // Specific origin
 //     credentials: true // Allow credentials
 //   }));
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000']
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000',  'https://jewels-ecommerce-frontend.onrender.com',
+  'https://jewels-ecommerce-admin.onrender.com']
 
 app.use(cors({
   origin: function (origin, callback) {
